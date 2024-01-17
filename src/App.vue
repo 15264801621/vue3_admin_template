ff0a4cb08f23959f4e2b2c3353ffc8b16a38e1b2
15264801621 <3409036981@qq.com>
2024-01-17 21:19:50 +0800

    20240117

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="box"></div>
+    <h1>我是app</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: red;
+  }
+}
+</style>
